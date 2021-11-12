45a094a4f459824bd50514f4595f45eb66ab7c22
Priscilla Baah <pyfbaah@gmail.com>
2021-11-12 04:16:49 +0000

    replace plotly table with dash data table

--- plots.py
+++ plots.py
@@ -1,7 +1,10 @@
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import dash_table as dt
+import numpy as np
 import pandas as pd
+import json
 
 def trend(df):
     trend_fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -57,37 +60,92 @@ def trend(df):
     )
     return trend_fig
 
-def matrix(df):
+# def matrix(df):
 
-    matrix_fig = go.Figure(data = [go.Table(
-        columnwidth=[120, 60, 60, 80],
-        header = dict(values=list(df.columns),
-                    fill_color="gray",
-                    line_color="gray",
-                    font = dict(color="white", size=10),
-                    height=22,
-                    align="left"),
-        cells =  dict(values=df.T, 
-                    fill_color="white",
-                    line = dict(color="gray", width=None),
-                    font = dict(color="black", size=8),
-                    height = 16,
-                    align="left")
-    )])
+    # matrix_fig = go.Figure(data = [go.Table(
+    #     columnwidth=[120, 60, 60, 80],
+    #     header = dict(values=list(df.columns),
+    #                 fill_color="gray",
+    #                 line_color="gray",
+    #                 font = dict(color="white", size=10),
+    #                 height=22,
+    #                 align="left"),
+    #     cells =  dict(values=df.T, 
+    #                 fill_color="white",
+    #                 line = dict(color="gray", width=None),
+    #                 font = dict(color="black", size=8),
+    #                 height = 16,
+    #                 align="left")
+    # )])
 
-    matrix_fig.update_layout(
-        autosize=True,
+    # matrix_fig.update_layout(
+    #     autosize=True,
+    #     margin=dict(
+    #         l=10,
+    #         r=50,
+    #         b=10,
+    #         t=10,
+    #         pad=4
+    #     )
+    # )
+    # return matrix_fig
+
+def create_map(df):
+    ghana_geo = json.load(open("ghana_regions.json","r"))
+    for feature in ghana_geo["features"]:
+        feature["id"] = feature["properties"]["NAME_1"]
+
+    map_fig = go.Figure()
+    labels = df["Profit"].apply(lambda x: "{:,.1f}k".format((x/1000)))
+    colors = np.where(df["Profit"]>300000,"rgb(3, 127, 62)", np.where(
+        df["Profit"]>100000,"rgb(53, 103, 157)", "rgb(214, 12, 12)"))
+
+    map_fig.add_trace(go.Choropleth(
+        geojson = ghana_geo,
+        locations = df["Region"],
+        featureidkey = "id",
+        locationmode = "geojson-id",
+        z = df["Profit"],
+        autocolorscale = False,
+        colorscale = [[0,'rgb(255, 255, 255)'],[1,'rgb(255, 255, 255)']],
+        showscale = False
+    ))
+
+    map_fig.add_trace(go.Scattergeo(
+        geojson = ghana_geo,
+        locations =df["Region"],
+        featureidkey = "id",
+        locationmode = "geojson-id",
+        mode = "markers+text",
+        text = labels,
+        marker = dict(
+                    size = df["Profit"]/10000,
+                    line_width = 0,
+                    color = colors
+                )
+    ))
+
+    map_fig.update_layout(
+        autosize=False,
         margin=dict(
-            l=10,
-            r=50,
-            b=10,
-            t=10,
-            pad=4
+            l=0,
+            r=0,
+            b=0,
+            t=0,
+            autoexpand = True
+        ),
+        width = 400,
+        height = 450,
+        geo = go.layout.Geo(
+            fitbounds = "geojson",
+            visible = False,
+            projection = dict(
+                type = "gnomonic"
+                # "stereographic" "gnomonic"
+            )
         )
     )
-    return matrix_fig
+    
 
-def create_map():
-    ghana_geo = pd.read_json("data/ghana_regions.json")
-    return ghana_geo
+    return map_fig
 
